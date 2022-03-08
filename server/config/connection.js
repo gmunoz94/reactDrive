cf94fb71bf49c2006c31adce292f44c14dcd98d5
Gerardo Munoz <g.munozii@icloud.com>
2022-03-08 13:07:42 -0600

    added jawsdb

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,19 +1,20 @@
 const Sequelize = require('sequelize');
 require('dotenv').config();
 
-const sequelize = new Sequelize(
-    // DB Name
+let sequelize;
+if (process.env.JAWSDB_URL) {
+  sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else {
+  sequelize = new Sequelize(
     process.env.DB_NAME,
-    // DB User
     process.env.DB_USER,
-    // DB Password
     process.env.DB_PASSWORD,
     {
-        // DB Location
-        host: 'localhost',
-        dialect: 'mysql',
-        port: 3306
-    }
-);
+      host: 'localhost', 
+      dialect: 'mysql', 
+      port: 3306,
+    },
+  );
+}
 
 module.exports = sequelize;
